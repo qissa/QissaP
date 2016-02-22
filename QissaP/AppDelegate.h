bcc2a8afd76a7816e558c09e1d31b6c00412a938
Esraa Aldeeb <edeeb@ccecentrix.com>
2016-02-22 11:40:38 +0200

    Initial Commit

---
+++ QissaP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QissaP
+//
+//  Created by Esraa Aldeeb on 2/22/16.
+//  Copyright © 2016 Esraa Aldeeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QissaP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QissaP
+//
+//  Created by Esraa Aldeeb on 2/22/16.
+//  Copyright © 2016 Esraa Aldeeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
